c98a6e6b6b8bb3f329fe116f2b2d5eba79c81586
Ashton Scott Snapp <ashton@ashtonsnapp.com>
2023-12-24 11:31:44 -0600

    Code written on phone did not actually work.
    I wish I could run cargo on iSH, but it doesn't work. So, until I got my
    laptop charger yesterday, I couldn't tell if the code would actually
    work. But, now I can, and hooo boy I messed up a lot.
    
    But, I fixed it now. And now I can publish v0.2.0 to crates.io!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! [ReadChars] is a simple iterator crate for turning the bytes of an I/O reader into characters
 //! for use by things like handwritten lexers.
 
+//--> Imports <--
+
 use std::{
     collections::VecDeque,
 	convert::From,
@@ -13,6 +15,8 @@ use std::{
 	str,
 };
 
+//--> Structs <--
+
 /// An iterator that takes any type implementing [std::io::Read] and loops over the UTF-8
 /// characters in it.
 ///
@@ -22,59 +26,73 @@ pub struct ReadChars<R: Read> {
     queue: VecDeque<char>,
 }
 
+//--> Functions <--
+
+impl<R: Read> ReadChars<R> {
+	/// The default capacity of the internal queue.
+	pub const DEFAULT_CAPACITY: usize = 16;
+
+	/// A constructor that allows you to specify a custom queue capacity.
+	/// If you are using the default capacity, it's easier to use [From].
+	pub fn new(reader: R, capacity: usize) -> ReadChars<R> {
+		ReadChars {
+			inner: reader.bytes(),
+			queue: VecDeque::with_capacity(capacity),
+		}
+	}
+}
+
 impl<R: Read> From<R> for ReadChars<R> {
 	fn from(value: R) -> Self {
 		ReadChars {
 			inner: value.bytes(),
-            queue: VecDeque::with_capacity(16),
+            queue: VecDeque::with_capacity(ReadChars::<R>::DEFAULT_CAPACITY),
 		}
 	}
 }
 
 impl<R: Read> Iterator for ReadChars<R> {
 	type Item = io::Result<char>;
-	
+
 	fn next(&mut self) -> Option<Self::Item> {
-        if self.queue.is_empty() {
-            while self.queue.len() < 16 {
-                let mut buf = [0u8; 4];
+		if self.queue.is_empty() {
+			let mut buffer = [0x0u8; 4];
 
-                buf[0] = match self.inner.next() {
-                    Some(Ok(b)) => b,
-                    Some(Err(e)) => return Some(Err(e)),
-                    None => break,
-                };
+			buffer[0] = match self.inner.next() {
+				Some(Ok(c)) => c,
+				Some(Err(e)) => return Some(Err(e)),
+				None => return None,
+			};
 
-                let need_bytes = {
-                    let start = buf[0];
+			let need_bytes = {
+				let start = buffer[0];
 
-                    if start.is_ascii() { 0 } else
-                    if start >> 5 == 0b110 { 1 } else
-                    if start >> 4 == 0b1110 { 2 } else
-                    if start >> 3 == 0b11110 { 3 } else {
-                        return Some(Err(io::Error::from(io::ErrorKind::InvalidData)))
-                    }
-                };
+				if start.is_ascii() { 0 } else
+				if start >> 5 == 0b110 { 1 } else
+				if start >> 4 == 0b1110 { 2 } else
+				if start >> 3 == 0b11110 { 3 } else
+				{ return Some(Err(io::Error::from(io::ErrorKind::InvalidData))) }
+			};
 
-                if need_bytes != 0 {
-                    for idx in 1..=need_bytes {
-                        buf[idx] = match self.inner.next() {
-                            Some(Ok(b)) if b >> 6 == 0b10 => b,
-                            Some(Ok(_)) => return Some(Err(io::Error::from(io::ErrorKind::InvalidData))),
-                            Some(Err(e)) => return Some(Err(e)),
-                            None => return Some(Err(io::Error::from(io::ErrorKind::InvalidData))),
-                        };
-                    }
-                }
+			if need_bytes != 0 {
+				for idx in 1..=need_bytes {
+					buffer[idx] = match self.inner.next() {
+						Some(Ok(byte)) if byte >> 6 == 0b10 => byte,
+						Some(Ok(_)) => return Some(Err(io::Error::from(io::ErrorKind::InvalidData))),
+						Some(Err(e)) => return Some(Err(e)),
+						None => return Some(Err(io::Error::from(io::ErrorKind::UnexpectedEof))),
+					}
+				}
+			}
 
-                let chr = match str::from_utf8(&buf[0..=need_bytes]) {
-                    Ok(c) => chr,
-                    Err(e) => return Some(Err(io::Error::new(io::ErrorKind::InvalidData, e))),
-                };
+			let chr = match str::from_utf8(&buffer[0..=need_bytes]) {
+				Ok(chr) => chr.chars().next().unwrap(),
+				Err(e) => return Some(Err(io::Error::new(io::ErrorKind::InvalidData, e))),
+			};
 
-                queue.push_back(chr);
-        }
+			self.queue.push_front(chr);
+		}
 
-        Some(Ok(queue.pop()?))
+		Some(Ok(self.queue.pop_back()?))
 	}
 }
